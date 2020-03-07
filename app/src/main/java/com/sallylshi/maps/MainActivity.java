f5c0a5976ae0340517843616ba787df2f90e0dfb
sallyshi <sally.l.shi@gmail.com>
2020-03-06 20:21:33 -0800

    Initial commit

---
+++ app/src/main/java/com/sallylshi/maps/MainActivity.java
@@ -0,0 +1,70 @@
+package com.sallylshi.maps;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 897897;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getJson();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void getJson() {
+        try {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to read the contacts
+                }
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant that should be quite unique
+
+                return;
+            }
+
+            FileInputStream Fin=new FileInputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/2020_MARCH.json");
+            byte[] b=new byte[1024];
+            StringBuilder origText = new StringBuilder();
+            int i;
+            while((i = Fin.read(b)) != -1) {
+                origText.append(new String(b));
+
+            }
+            Log.e("SALLY", i + "");
+            final String text = origText.toString();
+            TextView view = findViewById(R.id.test);
+
+            runOnUiThread( () ->view.setText(text));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
